2e6fb69e0c128b29d1a0ed900c943a81672286ee
Shiffa Ur Rehman <shiffa06@systemplus.co>
2023-12-11 11:55:11 +0500

    schema first approach

---
+++ src/book/book.resolver.ts
@@ -0,0 +1,16 @@
+import { Resolver, Query } from "@nestjs/graphql";
+
+
+@Resolver("Book")
+export class BookResolver{
+
+    @Query("books")
+    getAllBooks(){
+        return [
+            {id: 1, title: "Harry Potter", price: 500},
+            {id: 2, title: "Hunger Games", price: 600},
+            {id: 3, title: "Robin Hood", price: 900},
+        ]
+    }
+ 
+}
